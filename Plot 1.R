78475ad1bd763238712acd6535f5485505f382e2
GaryLiuLondon <garylgy@msn.com>
2014-05-09 18:02:23 -0400

    Create Plot 1.R

---
+++ Plot 1.R
@@ -0,0 +1,23 @@
+#all_data<-read.table(file="household_power_consumption.txt", sep=";",header=TRUE,colClasses=c("character","character",rep("numeric",7)),na.string="?")
+#data<-subset(all_data,all_data$Date %in% c("2007-02-01","2007-02-02"))
+# Instead of import all data and filter later (as shown above), I skipped unnecessary rows, only read specific rows (start from row 66637, total 2880 rows)
+
+# Data import
+
+data<-read.table(file="household_power_consumption.txt", sep=";",col.name=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),colClasses=c("character","character",rep("numeric",7)),na.string="?",skip=66637,nrows=2880)
+
+# Paste date and time to generate field with both date and time
+
+data$Time<-paste(data$Date,data$Time,sep=" ")
+
+# Convert character to datetime and date
+
+data$Time<-strptime(data$Time,format="%d/%m/%Y %H:%M:%S")
+data$Date<-as.Date(data$Date,format="%d/%m/%Y")
+
+# Plotting histogram
+
+png(file="plot1.png",width=480, height=480)
+
+hist(data$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
+dev.off()
